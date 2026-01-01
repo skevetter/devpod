e9e106bb7cb7ca7f01d37ec4f76f3e6b51dd4e7d
Samuel K <skevetter@pm.me>
2026-01-01 01:11:54 +0000

    fixup!: secure workflow
    Signed-off-by: Samuel K <skevetter@pm.me>

--- e2e/tests/up/up.go
+++ e2e/tests/up/up.go
@@ -171,39 +171,6 @@ var _ = DevPodDescribe("devpod up test suite", func() {
 			framework.ExpectNoError(err)
 		})
 
-		ginkgo.It("should allow checkout of a private GitRepo", ginkgo.Label("git", "private"), func() {
-			username := os.Getenv("GH_USERNAME")
-			token := os.Getenv("GH_ACCESS_TOKEN")
-
-			ctx := context.Background()
-			f, err := setupDockerProvider(initialDir+"/bin", "docker")
-			framework.ExpectNoError(err)
-
-			// setup git credentials
-			err = exec.Command("git", []string{"config", "--global", "credential.helper", "store"}...).Run()
-			framework.ExpectNoError(err)
-
-			gitCredentialString := []byte("https://" + username + ":" + token + "@github.com")
-			err = os.WriteFile(
-				filepath.Join(os.Getenv("HOME"), ".git-credentials"),
-				gitCredentialString, 0o644)
-			framework.ExpectNoError(err)
-			defer func() { _ = os.Remove(filepath.Join(os.Getenv("HOME"), ".git-credentials")) }()
-
-			name := "testprivaterepo"
-			ginkgo.DeferCleanup(f.DevPodWorkspaceDelete, context.Background(), name)
-
-			// Wait for devpod workspace to come online (deadline: 30s)
-			err = f.DevPodUp(ctx, "https://github.com/"+username+"/test_private_repo.git")
-			framework.ExpectNoError(err)
-
-			// Ensure git credentials are properly forwarded by cloning the private repo
-			// from within the container
-			out, err := f.DevPodSSH(ctx, name, "git clone https://github.com/"+username+"/test_private_repo")
-			framework.ExpectNoError(err)
-			fmt.Println(out)
-		})
-
 		ginkgo.It("run devpod in Kubernetes", ginkgo.Label("provider-kubernetes"), func() {
 			ctx := context.Background()
 			f := framework.NewDefaultFramework(initialDir + "/bin")
