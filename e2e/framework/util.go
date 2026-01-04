112eb867bbd2729fa07c11675dbd1ca1f9816c8f
Samuel K <skevetter@pm.me>
2026-01-03 22:45:50 -0700

    test(e2e/framework): remove copy package for std lib (#233)
    Signed-off-by: Samuel K <skevetter@pm.me>

--- e2e/framework/util.go
+++ e2e/framework/util.go
@@ -2,13 +2,12 @@ package framework
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
 	"time"
-
-	"github.com/otiai10/copy"
 )
 
 func GetTimeout() time.Duration {
@@ -25,36 +24,21 @@ func GetTimeout() time.Duration {
 }
 
 func CreateTempDir() (string, error) {
-	// Create temporary directory
-	dir, err := os.MkdirTemp("", "temp-*")
-	if err != nil {
-		return "", err
-	}
-
-	// Make sure temp dir path is an absolute path
-	dir, err = filepath.EvalSymlinks(dir)
-	if err != nil {
-		return "", err
-	}
-
-	return dir, nil
+	return createTempDir("")
 }
 
 func CopyToTempDirWithoutChdir(relativePath string) (string, error) {
-	// Create temporary directory
-	dir, err := os.MkdirTemp("", "temp-*")
+	dir, err := createTempDir("")
 	if err != nil {
 		return "", err
 	}
 
-	// Make sure temp dir path is an absolute path
-	dir, err = filepath.EvalSymlinks(dir)
+	absPath, err := filepath.Abs(relativePath)
 	if err != nil {
 		return "", err
 	}
 
-	// Copy the file files from relativePath to the temp dir
-	err = copy.Copy(relativePath, dir)
+	err = copyDir(absPath, dir)
 	if err != nil {
 		_ = os.RemoveAll(dir)
 		return "", err
@@ -64,26 +48,22 @@ func CopyToTempDirWithoutChdir(relativePath string) (string, error) {
 }
 
 func CopyToTempDirInDir(baseDir, relativePath string) (string, error) {
-	// Create temporary directory
-	dir, err := os.MkdirTemp(baseDir, "temp-*")
+	absPath, err := filepath.Abs(relativePath)
 	if err != nil {
 		return "", err
 	}
 
-	// Make sure temp dir path is an absolute path
-	dir, err = filepath.EvalSymlinks(dir)
+	dir, err := createTempDir(baseDir)
 	if err != nil {
 		return "", err
 	}
 
-	// Copy the file files from relativePath to the temp dir
-	err = copy.Copy(relativePath, dir)
+	err = copyDir(absPath, dir)
 	if err != nil {
 		_ = os.RemoveAll(dir)
 		return "", err
 	}
 
-	// Set the temp director as the current directory
 	err = os.Chdir(dir)
 	if err != nil {
 		_ = os.RemoveAll(dir)
@@ -110,4 +90,101 @@ func CleanupTempDir(initialDir, tempDir string) {
 func CleanString(input string) string {
 	input = strings.ReplaceAll(input, "\\", "")
 	return strings.ReplaceAll(input, "/", "")
+}
+
+// createTempDir creates a temporary directory based on environment and base directory
+func createTempDir(baseDir string) (string, error) {
+	var dir string
+	var err error
+
+	if os.Getenv("GITHUB_ACTIONS") == "true" {
+		runnerTemp := os.Getenv("RUNNER_TEMP")
+		if runnerTemp != "" {
+			dir, err = os.MkdirTemp(runnerTemp, "temp-*")
+		} else {
+			dir, err = os.MkdirTemp("", "temp-*")
+		}
+	} else if os.Getenv("ACT") == "true" {
+		dir, err = os.MkdirTemp("/tmp", "temp-*")
+	} else {
+		if baseDir == "" {
+			dir, err = os.MkdirTemp("", "temp-*")
+		} else {
+			dir, err = os.MkdirTemp(baseDir, "temp-*")
+		}
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	// ensure temp dir path is an absolute path
+	return filepath.EvalSymlinks(dir)
+}
+
+func copyDir(src, dst string) error {
+	srcInfo, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !srcInfo.IsDir() {
+		return copyFile(src, filepath.Join(dst, filepath.Base(src)))
+	}
+
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+		dstPath := filepath.Join(dst, relPath)
+
+		if info.Mode()&os.ModeSymlink != 0 {
+			linkTarget, err := os.Readlink(path)
+			if err != nil {
+				return err
+			}
+			return os.Symlink(linkTarget, dstPath)
+		}
+
+		if info.IsDir() {
+			return os.MkdirAll(dstPath, info.Mode().Perm())
+		}
+
+		return copyFile(path, dstPath)
+	})
+}
+
+func copyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = srcFile.Close() }()
+
+	info, err := srcFile.Stat()
+	if err != nil {
+		return err
+	}
+	mode := info.Mode().Perm()
+
+	if err := os.MkdirAll(filepath.Dir(dst), 0755); err != nil {
+		return err
+	}
+
+	dstFile, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, mode)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = dstFile.Close() }()
+
+	if _, err := io.Copy(dstFile, srcFile); err != nil {
+		return err
+	}
+
+	return os.Chmod(dst, mode)
 }
--- go.mod
+++ go.mod
@@ -39,7 +39,6 @@ require (
 	github.com/moby/term v0.5.2
 	github.com/onsi/ginkgo/v2 v2.27.3
 	github.com/onsi/gomega v1.38.3
-	github.com/otiai10/copy v1.14.1
 	github.com/pkg/errors v0.9.1
 	github.com/pkg/sftp v1.13.10
 	github.com/ramr/go-reaper v0.3.1
@@ -208,7 +207,6 @@ require (
 	github.com/olekukonko/tablewriter v1.1.2 // indirect
 	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/opencontainers/runtime-spec v1.2.1 // indirect
-	github.com/otiai10/mint v1.6.3 // indirect
 	github.com/pires/go-proxyproto v0.8.1 // indirect
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
