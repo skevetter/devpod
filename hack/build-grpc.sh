63431021a01ac205d4ca2b76dbf95d4fbeff7a76
Samuel K <skevetter@pm.me>
2026-01-11 02:20:42 +0000

    chore: replace gRPC build script with task
    Signed-off-by: Samuel K <skevetter@pm.me>

--- hack/build-grpc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# sudo apt install protobuf-compiler
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-
-cd pkg/agent/tunnel/ || exit
-protoc -I . tunnel.proto --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative
