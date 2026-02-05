0f78691a74ea9716fa76ab937bc442957c9f353b
Samuel K <skevetter@pm.me>
2026-02-05 17:49:19 +0000

    fix: formatting
    Signed-off-by: Samuel K <skevetter@pm.me>

--- pkg/inject/inject.sh
+++ pkg/inject/inject.sh
@@ -18,11 +18,14 @@ EXISTS_CHECK="{{ .ExistsCheck }}"
 TEMP_PATH="$INSTALL_PATH.$$"
 
 command_exists() { command -v "$@" >/dev/null 2>&1; }
-fail() { echo >&2 "Error: $1"; return 1; }
+fail() {
+    echo >&2 "Error: $1"
+    return 1
+}
 
 is_arm() {
     case "$(uname -a)" in
-        *arm*|*aarch*) return 0 ;;
+        *arm* | *aarch*) return 0 ;;
         *) return 1 ;;
     esac
 }
@@ -54,23 +57,23 @@ handshake() {
 check_noexec() {
     mount_path="$(df -P "$INSTALL_DIR" 2>/dev/null | tail -n 1 | awk '{print $NF}')"
     [ -z "$mount_path" ] && return 0
-    mount | grep -F "on ${mount_path} " | grep -q noexec && \
+    mount | grep -F "on ${mount_path} " | grep -q noexec &&
         fail "installation directory $INSTALL_DIR is noexec, please choose another location"
     return 0
 }
 
 can_write_without_privilege() {
-    mkdir -p "$INSTALL_DIR" 2>/dev/null && \
-    touch "$INSTALL_PATH" 2>/dev/null && \
-    chmod +x "$INSTALL_PATH" 2>/dev/null && \
-    rm -f "$INSTALL_PATH" 2>/dev/null
+    mkdir -p "$INSTALL_DIR" 2>/dev/null &&
+        touch "$INSTALL_PATH" 2>/dev/null &&
+        chmod +x "$INSTALL_PATH" 2>/dev/null &&
+        rm -f "$INSTALL_PATH" 2>/dev/null
 }
 
 detect_privilege_command() {
     if can_write_without_privilege; then
         echo "sh -c"
     elif command_exists sudo; then
-        sudo -nl >/dev/null 2>&1 || \
+        sudo -nl >/dev/null 2>&1 ||
             fail "sudo requires a password and no password is available. Please ensure your user account is configured with NOPASSWD"
         echo "sudo -E sh -c"
     else
