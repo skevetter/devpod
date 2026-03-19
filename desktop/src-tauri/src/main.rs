aa217bca7bda1925fa17594374f54b40f5a52ae6
Duke Lee <github@krnhotwings.com>
2026-03-18 19:31:28 -0700

    fix(deps): Added tauri plugin-opener
    Replacing plugin-shell file/path/URL opening with plugin-opener

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -97,6 +97,7 @@ fn main() -> anyhow::Result<()> {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_shell::init())
