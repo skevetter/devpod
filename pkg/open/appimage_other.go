820648d433132d2b08a8b7e296d9eb3ef805d615
Samuel K <skevetter@pm.me>
2026-04-11 12:07:05 -0500

    fix: return error instead of panicking in non-Linux openURLSanitized stub

--- pkg/open/appimage_other.go
+++ pkg/open/appimage_other.go
@@ -2,10 +2,12 @@
 
 package open
 
+import "errors"
+
 func isAppImage() bool {
 	return false
 }
 
 func openURLSanitized(_ string) error {
-	panic("openURLSanitized is only available on Linux")
+	return errors.New("openURLSanitized is only available on Linux")
 }
